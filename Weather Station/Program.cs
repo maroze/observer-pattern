7aadcb7f43bd9dc7ca60b3a3aa114cda38ac4826
maroze <91451262+maroze@users.noreply.github.com>
2023-04-10 15:28:50 +0300

    update

--- Weather Station/Program.cs
+++ Weather Station/Program.cs
@@ -10,24 +10,30 @@ namespace Weather_Station
     {
         static void Main(string[] args)
         {
+            //Создание субъекта
             ConcreteSubject subject = new ConcreteSubject();
+
+            //Создание наблюдателей
             ConcreteObserver observer1 = new ConcreteObserver();
             ConcreteObserver observer2 = new ConcreteObserver();
 
+            //Регистрация первого наблюдателя и изменение состояния
             subject.registerObserver(observer1);
             subject.State = "abc";
-            Console.WriteLine(observer1.Count);
-            Console.WriteLine(observer2.Count);
+            Console.WriteLine("Оповещения 1-го наблюдателя: " + observer1.Count);
+            Console.WriteLine("Оповещения 2-го наблюдателя: " + observer2.Count);
 
+            //Регистрация второго наблюдателя и изменение состояния
             subject.registerObserver(observer2);
             subject.State = "def";
-            Console.WriteLine(observer1.Count);
-            Console.WriteLine(observer2.Count);
+            Console.WriteLine("Оповещения 1-го наблюдателя: " + observer1.Count);
+            Console.WriteLine("Оповещения 2-го наблюдателя: " + observer2.Count);
 
+            //Удаление второго наблюдателя и изменение состояния
             subject.removeObserver(observer2);
             subject.State = "g";
-            Console.WriteLine(observer1.Count);
-            Console.WriteLine(observer2.Count);
+            Console.WriteLine("Оповещения 1-го наблюдателя: " + observer1.Count);
+            Console.WriteLine("Оповещения 2-го наблюдателя: " + observer2.Count);
 
 
             Console.ReadLine();
